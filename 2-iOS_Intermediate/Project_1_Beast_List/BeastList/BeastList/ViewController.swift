d08dec064708e3844845cccaf4265b9fcfc3f256
James Hunt <jhunt@techtalentsouth.com>
2021-07-01 16:05:54 -0400

    restructure

---
+++ 2-iOS_Intermediate/Project_1_Beast_List/BeastList/BeastList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeastList
+//
+//  Created by James Hunt on 7/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
